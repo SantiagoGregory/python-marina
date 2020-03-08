c55d687464d486493ea44d57f8c87cb82de68466
SantiagoGregory <santiagogregoryl@gmail.com>
2020-03-08 09:52:44 -0400

    Added solution

---
+++ solution1.py
@@ -0,0 +1,14 @@
+# Fix the following statements so that the program prints "abc."
+
+result = ""
+
+if True and True:
+    result+="a"
+
+if 5 > 4:
+    result+="b"
+
+if 5 // 3 == 1:
+    result+="c"
+
+print(result)
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $(python solution1.py) = "abc" ]
+then
+    echo 1
+else
+    echo 0
+fi
